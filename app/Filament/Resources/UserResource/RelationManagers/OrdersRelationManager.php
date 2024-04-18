a894e74a622573a68a2a86afa8c90f9c3fa1bcd5
ibrahim <e.mohmed55@gmail.com>
2024-04-18 16:03:12 +0200

    user realtion with orders

---
+++ app/Filament/Resources/UserResource/RelationManagers/OrdersRelationManager.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\RelationManagers;
+
+use App\Filament\Resources\OrderResource;
+use App\Models\Order;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrdersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'orders';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                    //
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+                TextColumn::make('id')
+                ->label('Order ID')
+                ->searchable(),
+
+                TextColumn::make('grand_total')
+                ->money('USD'),
+
+                TextColumn::make('stats')
+                ->badge()
+                ->color(fn(string $state):string=>
+                match( $state){
+                    'new' => 'info',
+                    'processing' => 'warning',
+                    'shipped' => 'success',
+                    'delivered' => 'success',
+                    'cancelled' => 'danger',
+                })
+                ->icon(fn(string $state):string=>match( $state){
+                    'new' => 'heroicon-m-sparkles',
+                    'processing' => 'heroicon-m-arrow-path',
+                    'shipped' => 'heroicon-m-truck',
+                    'delivered' => 'heroicon-m-check-badge',
+                    'cancelled' => 'heroicon-m-x-circle',
+                })
+                ->sortable(),
+                TextColumn::make('pyment_method')
+                ->sortable()
+                ->searchable(),
+
+                TextColumn::make('pyment_status')
+                ->sortable()
+                ->badge()
+                ->searchable(),
+
+                TextColumn::make('created_at')
+                ->label('Order Date')
+                ->dateTime()
+                ->sortable()
+                ->searchable(),
+
+
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+               // Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Action::make('View Order')->url(fn(Order $record) => OrderResource::getUrl('view', ['record' => $record]))
+                ->color('info')
+                ->icon('heroicon-o-eye'),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
